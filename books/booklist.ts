e7f5a38df0bd32447060e6488108509bc81512c5
Mao <m@maowtm.org>
2022-06-16 18:27:57 +0100

    Add more books

--- books/booklist.ts
+++ books/booklist.ts
@@ -79,7 +79,8 @@ export const getBookList = () => ({
       url: "https://nostarch.com/rust-rustaceans",
       description: `
         <p><i><b>Rust for Rustaceans</b> &mdash; Idiomatic Programming for Experienced Developers</i> is a great book on
-        intermediate-to-advanced Rust topics. It is written by Jon Gjengset, who has a YouTube channel and does a lot of
+        intermediate-to-advanced Rust topics. It is written by Jon Gjengset, who has a
+        <a target="_blank" href="https://www.youtube.com/c/JonGjengset">YouTube channel</a> and does a lot of
         Rust-coding streams, touching various advanced topics like lock-free programming.
       `
     }),
@@ -90,6 +91,39 @@ export const getBookList = () => ({
       description: `
         <p>This is a light read on certain aspects of European history from the Roman Empire era to
         pre-modern times, covering topics like forms of government, language, religion, argiculture, etc.
+      `
+    }),
+    new BookObject(require("url:../books/img/TheDisappearingSpoon.png"), 0.9, 0.9, 1, {
+      title: "The Disappearing Spoon",
+      author: "Sam Kean",
+      url: "https://www.goodreads.com/book/show/9800230-the-disappearing-spoon",
+      description: `
+        <p><b>The Disappearing Spoon</b>: And Other True Tales of Madness, Love, and the History of the World from the Periodic Table of the Elements
+
+        <p>This is a book about some interesting history of chemistry and science in general. A bit long for my taste but still enjoyable.
+      `
+    }),
+    new BookObject(require("url:../books/img/SurelyYoureJoking.png"), 0.7, 1, 1, {
+      title: "Surely You're Joking, Mr. Feynman!",
+      author: "Richard P. Feynman",
+      url: "https://wwnorton.com/books/Surely-Youre-Joking-Mr-Feynman/",
+      description: `
+        <p><b>Richard P. Feynman</b> is an inspiring physicist and Nobel Prize winner. In this book, he described his
+        philosophy around scientific communication in a series of light-hearted stories, and is quite fun to read.
+      `
+    }),
+    new BookObject(require("url:../books/img/TrueNorth.png"), 0.7, 0.9, 0.9, {
+      title: "True North",
+      author: "Bruce Henderson",
+      url: "https://www.goodreads.com/book/show/107425.True_North",
+      description: `
+        <p><b>True North</b>: Peary, Cook, and the Race to the Pole
+
+        <p>This was one of the book
+        <a target="_blank" href="https://www.youtube.com/watch?v=Roq20IV4yP0">recommended by MinutePhysics</a>!
+        It lays out the story of the arctic exploration done by Frederick Cook, as well as discussing some of the controversies
+        regarding who actually reached the North Pole first. However I find that the author perhaps sided a bit too closely with Cook
+        in this matter.
       `
     }),
   ]
